1832ace526e1d1e066d956e3aeec24130a2eae7d
wellington chida <wellingtonchida@protonmail.com>
2023-08-14 08:01:45 -0300

    🎉 Firist Commit

---
+++ src/Models/User.cs
@@ -0,0 +1,8 @@
+namespace Namespace;
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+}
---
+++ src/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+namespace Namespace;
+public class UserRepository
+{
+    public User Get(string userName, string password)
+    {
+        //Mocking the user data
+        var user = new List<User>
+        {
+            new User { Id = 1, Name = "admin", Password = "admin", Role = "admin" },
+            new User { Id = 2, Name = "user", Password = "user", Role = "user" }
+        };
+
+        return user.Where(x => x.Name.ToLower() == userName.ToLower() && x.Password == password).FirstOrDefault();
+    }
+    
+}
